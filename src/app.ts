f9409f8c886acd06b71311193af93eacf9202806
sslhansen <sebastian@lundby.net>
2021-03-18 12:02:14 +0100

    Adding some things

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,30 @@ import dotenv from "dotenv";
 import path from "path"
 dotenv.config()
 const app = express()
+import friendRoutes from "./routes/FriendRoutes"
+const debug = require("debug")("app")
 
+app.use((req, res, next) => {
+  debug(new Date().toLocaleDateString() + req.method + req.url + req.ip);
+  next()
+})
 
 //app.use('/static', express.static('public'))
 app.use(express.static(path.join(process.cwd(), "public")))
 
 
-app.get("/demo", (req, res) => {
+app.use("api/friends", friendRoutes)
 
+app.get("/demo", (req, res) => {
   res.send("Server is up");
 })
+
+app.use((req, res, next)=>{
+  res.status(404).json({errorCode: 404, msg: "not found"})
+})
+
+
+
 export default app;
 
 
---
+++ src/errors/apiError.ts
@@ -0,0 +1,23 @@
+/*
+  See this article for details about custom errors:
+  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
+*/
+
+// ADD THIS FILE TO A FOLDER errors 
+
+class ApiError extends Error {
+    constructor(msg:string,public errorCode ?:number) {
+      super(msg)
+  
+      // Maintains proper stack trace for where our error was thrown (only available on V8)
+      if (Error.captureStackTrace) {
+        Error.captureStackTrace(this, ApiError)
+      }
+  
+      this.name = 'ApiError'
+      this.errorCode = errorCode || 500;
+      
+    }
+}
+
+export {ApiError}
